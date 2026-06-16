795b7b04a93b8549bce55341d44a474c0a602a57
Mert <mertkanfe@hotmail.com>
2026-06-16 23:21:10 +0300

    javascript fundamentals updated

---
+++ 03-Javascript-Fundamentals/notlar/019-fonksiyonlar-neden-fonksiyonlara-ihtiyac-duyariz-ve-ilk-fonksiyonumuz.js
@@ -0,0 +1,13 @@
+// FONKSİYONLAR
+// Fonksiyon Nedir? Neden Kullanırız? #19
+
+function helloWorld() {
+    console.log("Hello World")
+}
+
+function hello() {
+    console.log("Merhaba")
+    helloWorld()
+}
+
+hello() // calistir
