79d0bb5cf5cc8e0a50609ccc6200212cb481cc84
Mert <mertkanfe@hotmail.com>
2026-06-23 23:37:13 +0300

    javascript fundamentals updated

---
+++ 03-Javascript-Fundamentals/notlar/041-object-icinde-metot-nasil-ekleriz.js
@@ -0,0 +1,14 @@
+// Nesnelere Nasıl Metot Ekleriz? #41
+
+let user1 = {
+  firstName: "Bilgin",
+  lastName: "Uzman",
+  score: [1, 2, 3, 4], 
+  isActive: true,
+  shortName: function() {
+    return `${this.firstName[0].toUpperCase()}. ${this.lastName}`
+  }
+};
+
+console.log(user1)
+console.log(user1.shortName)
